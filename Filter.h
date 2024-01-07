d000adf3009b07eb20b7b1d6c6c1b73d85a4c12f
Spl3tnikk <okgj5@yandex.ru>
2024-01-07 23:22:34 +0300

    add set path + filter

---
+++ Filter.h
@@ -0,0 +1,14 @@
+//
+// Created by lolo7 on 07.01.2024.
+//
+
+#ifndef MEMORYVISUALIZER_FILTER_H
+#define MEMORYVISUALIZER_FILTER_H
+
+
+class Filter {
+
+};
+
+
+#endif //MEMORYVISUALIZER_FILTER_H
--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include "DirectoryReader.cpp"
 #include "SizeReader.cpp"
+#include "Filter.cpp"
 
 int main() {
-    char disk;
+    std::string disk;
+    std::cout << "Enter path: ";
     std::cin >> disk;
     std::vector<std::string> directories = DirReader::DirectoryReader(disk);
-    double bToGB = 1024*1024*1024;
     for (const std::string& i : directories) {
-        double size = SizeReader::DirSize(i)/bToGB;
-        if (size > 0)  {
-            std::cout << i << " "<< size << " GB\n";
+        double size = SizeReader::DirSize(i);
+        if (size >= 0)  {
+            ChangeScope::Scoper(size, i);
         }
     }
 }
